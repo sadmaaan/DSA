e9da6a0e0c28e1ddcdd2dc6b2a0790512177cae5
Sadman Sakib <sadm6n@gmail.com>
2022-11-05 07:15:09 +0600

    backtracking all path print with matrix

---
+++ backtracking_all_path_print_with_matrix.py
@@ -0,0 +1,41 @@
+def all_path(r, c, p, maze, path, step):
+    if r == len(maze) - 1 and c == len(maze[0]) - 1:
+        path[r][c] = step
+        for i in path:
+            print('\t'.join(map(str, i)))
+            # print(i)
+        print(p)
+        print("\n")
+        return
+    
+    if not maze[r][c]:
+        return
+    
+    maze[r][c] = False # backtracking -> making false during recursive call
+    path[r][c] = step
+
+    # if r < len(mage) - 1 and c < len(mage) - 1:
+    #     all_path(r + 1, c + 1, p + 'd', mage)
+    if r < len(maze) - 1:
+        all_path(r + 1, c, p + 'D', maze, path, step + 1)
+    if c < len(maze[0]) - 1:
+        all_path(r, c + 1, p + 'R', maze, path, step + 1)
+    if r > 0:
+        all_path(r - 1, c, p + 'U', maze, path, step + 1)
+    if c > 0:
+        all_path(r, c - 1, p + 'L', maze, path, step + 1)
+
+    maze[r][c] = True # backtracking -> making true when returning
+    path[r][c] = 0
+        
+
+
+maze = [
+    [True, True, True],
+    [True, True, True],
+    [True, True, True]
+]
+
+w, h = len(maze), len(maze[0])
+path = [[0 for x in range(w)] for y in range(h)] 
+all_path(0, 0, "", maze, path, 1)
